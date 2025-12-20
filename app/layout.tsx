ab19b0071c88f5c1f9f82efabfb0b8da10798129
samnang23 <samnangkeo.io.23@gmail.com>
2025-12-20 13:20:44 +0700

    add icon favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="km" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/images/snr3.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
